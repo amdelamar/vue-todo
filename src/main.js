37179d79dbd448a46aa365562bedb78711499874
amdelamar <amdelamar@protonmail.com>
2018-01-03 15:33:28 -0500

    Add vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App.vue'
+import Home from './components/Home.vue'
+import New from './components/New.vue'
+import Edit from './components/Edit.vue'
+
+const router = new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: Home
+  }, {
+    path: '/new',
+    name: 'new',
+    props: true,
+    component: New
+  }, {
+    path: '/edit/:todo',
+    name: 'edit',
+    props: true,
+    component: Edit
+  }]
+})
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
